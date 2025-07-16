b6089a7597e5c608ed67e23c21be54a912065d17
L3on1kL <igorkoynov_2001@gmail.com>
2025-07-17 04:03:59 +0700

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.l3on1kl.movies"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
